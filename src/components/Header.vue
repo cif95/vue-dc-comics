57d87cbca479d1c9270c1b6b8391315cc4a755ce
cif95 <vivianacifarelli9@gmail.com>
2022-03-28 17:21:07 +0200

    implemented header

---
+++ src/components/Header.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="home-header">
+    <div class="container">
+      <div class="header-logo">
+        <img src="../assets/img/dc-logo.png" alt="dc comics logo" />
+      </div>
+      <nav>
+        <ul>
+          <li v-for="(link, index) in headerNavLinks" :key="index">
+            <a href="#">{{ link.text }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader",
+  data: function () {
+    return {
+      headerNavLinks: [
+        {
+          text: "Characters",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Comics",
+          active: true,
+          url: "#",
+        },
+        {
+          text: "Movies",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Tv",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Games",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Collectibles",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Videos",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Fans",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "News",
+          active: false,
+          url: "#",
+        },
+        {
+          text: "Shop",
+          active: false,
+          url: "#",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+div.container {
+  width: 70%;
+  padding: 1rem;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: space-between;
+  img {
+    width: 75%;
+  }
+  li {
+    list-style-type: none;
+    display: inline-block;
+    margin-right: 1.2rem;
+    font-size: 0.8rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    a {
+      color: rgb(24, 24, 24);
+      text-decoration: none;
+    }
+  }
+}
+</style>
