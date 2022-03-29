c7e3613f3705b614375a3ff47dbc5dab6967ae46
cif95 <vivianacifarelli9@gmail.com>
2022-03-29 16:21:33 +0200

    implemented cards

--- src/components/main/MainContentCard.vue
+++ src/components/main/MainContentCard.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <div class="card">
+    <img :src="cardObject.thumb" :alt="cardObject.series" />
+    <h5>{{ cardObject.series }}</h5>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MainContentCard",
+  props: { cardObject: Object },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../../assets/scss/partials/_variables.scss";
+div.card {
+  border: 1rem solid $secondaryColor;
+  width: calc(100% / 6 - 2rem);
+  img {
+    height: 200px;
+    width: 200px;
+    object-fit: cover;
+    object-position: 20% 10%;
+  }
+}
 </style>
